04c141dd330478f87f3dfe3c109c38d8133ccd17
msf <1017629235@qq.com>
2021-07-09 14:10:14 +0800

    前缀和增加

--- Pyrthon/src/前缀和.py
+++ Pyrthon/src/前缀和.py
@@ -16,3 +16,20 @@ def subarraySum(nums: List[int], k: int) -> int:
 
 
 print(subarraySum([1], 0))
+
+
+def numberOfSubarrays(nums: List[int], k: int) -> int:
+    map = {0: 1}
+    sum = 0
+    res = 0
+    for i in nums:
+        if i % 2 == 1: sum += 1
+        if sum-k in map: res += map[k - sum]
+        if sum in map:
+            map[sum] += 1
+        else:
+            map[sum] = 1
+    return res
+
+
+print(numberOfSubarrays([1, 1, 2, 1, 1], 3))
