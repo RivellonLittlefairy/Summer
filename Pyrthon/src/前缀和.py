3399e01d944d9c5f3de0c07c6fa1e3fd39567e67
msf <1017629235@qq.com>
2021-07-09 17:02:49 +0800

    工厂方法模式

--- Pyrthon/src/前缀和.py
+++ Pyrthon/src/前缀和.py
@@ -1,6 +1,10 @@
 from typing import List
 
-
+#前缀和是用来求一段连续区间值等于目标的算法
+#常用于求数组的定值子数组和或者树的某个区间之和为定值
+#思想是一次遍历数组（树）用map存下0-1，0-2，0-3的所有值
+#再判断map中，也就是之前是否出现过前缀和为target-目前和的值，
+#如果有，则 target-当前和 对应的值即为符合条件且尾为目前坐标的区间数量
 def subarraySum(nums: List[int], k: int) -> int:
     map = {0: 1}
     sum = 0
--- 设计模式/Factory/FactoryMain.java
+++ 设计模式/Factory/FactoryMain.java
@@ -1,4 +1,56 @@
 package Factory;
 
+import javax.xml.stream.FactoryConfigurationError;
+
 public class FactoryMain {
+    public static void main(String[] args) {
+        //简单工厂模式调用
+        SimpleFactory factory1=new SimpleFactory();
+        Product p1=factory1.creatProduct("IDCard");
+        p1.use();
+
+        //工厂方法模式调用
+        Factory factory2=new IDCardFactory();
+        Product p2=factory2.creat();
+        p2.use();
+    }
 }
+//先写一个简单工厂模式，在这个模式中只有一个工厂负责生产所有的实例
+class SimpleFactory{
+    public Product creatProduct(String name){
+        //这里体现了简单工厂模式的缺点，每次增加产品都要对工厂类进行修改，不符合开闭原则
+        if(name=="IDCard") return new IDCard("simple factory card");
+
+
+        throw new FactoryConfigurationError("产品名输入错误");
+    }
+    //也可以为每个产品写一个生产的方法，而不是集成到一个方法中进行判断
+}
+//接下来实现工厂方法模式，这种策略通过为每一个产品设置一个独有的工厂，从而避免了修改代码的情况出现
+//这种方法的缺点是每有一个产品，就要为其设计一个工厂类，导致系统冗杂类太多
+abstract class Factory{
+    abstract Product creat();
+}
+class IDCardFactory extends Factory{
+    @Override
+    Product creat() {
+        return new IDCard("factory method card");
+    }
+}
+//定义产品的抽象类，所有产品均要继承此抽象类
+abstract class Product{
+    abstract void use();
+}
+class IDCard extends Product{
+    String name;
+
+    public IDCard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    void use() {
+        System.out.println("IDCard "+name+" be used");
+    }
+}
+
